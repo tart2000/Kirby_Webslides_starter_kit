9d28809d26f01ec219702de2b743b9221f021b19
Arthur Schmitt <tart2000design@gmail.com>
2017-04-24 15:37:23 +0200

    working portfolio slide type

---
+++ site/snippets/portfolio-slide.php
@@ -0,0 +1,27 @@
+<section class="aligncenter">
+    <div class="wrap">
+
+      <h2><?= $s->title() ?></h2>
+
+      <?= $s->description()->kirbytext() ?>
+
+      <?php if ($s->items() != '') : ?>
+        <ul class="flexblock gallery">
+          <?php foreach($s->items()->toStructure() as $item): ?>
+            <li>
+              <a href="<?= $item->theurl() ?>">
+                <figure>
+                  <img src="<?= $s->image($item->img())->url() ?>">
+                  <figcaption>
+                    <h2><?= $item->title() ?></h2>
+                  </figcaption>
+                </figure>
+              </a>
+            </li>
+          <?php endforeach ?>
+        </ul>
+      <?php endif ?>
+
+    </div>
+    <!-- .end .wrap -->
+  </section>
--- site/templates/slideshow.php
+++ site/templates/slideshow.php
@@ -1,24 +1,17 @@
 <?php snippet('header') ?>
 
 <article id="webslides">  <!-- Slideshow? id="webslides" -->
-  <section class="bg-black-blue aligncenter">
-   <span class="background dark" style="background-image:url('https://source.unsplash.com/6njoEbtarec/')"></span>
-    <!--.wrap = container (width: 90%) -->
-    <div class="wrap">
-      <p class="text-subtitle">WebSlides Tutorial</p>
-      <h1 class="text-landing">Components</h1>
-      <p class="text-symbols">* * *</p>
-      <p>
-        <a class="button ghost" href="https://github.com/webslides/webslides" title="Download WebSlides">
-          <svg class="fa-github">
-            <use xlink:href="#fa-github"></use>
-          </svg>
-          Free Download
-        </a>
-      </p>
-    </div>
-    <!-- .end .wrap -->
-  </section>
+
+  <?php foreach ($page->children() as $s) : ?>
+
+    <?php if ($s->template() == 'portfolio-slide') : ?> 
+      <?php snippet('portfolio-slide', array('s' => $s)) ?>
+    <?php endif ?>
+
+  <?php endforeach ?>
+
+
+
 
   <section class="fullscreen">
     <div class="card-50">
@@ -47,53 +40,7 @@
     <!-- end .card-50-->
   </section>
 
-  <section class="aligncenter">
-    <div class="wrap">
-      <ul class="flexblock gallery">
-        <li>
-          <a href="../demos/why-webslides.html" title="Why WebSlides?">
-            <figure>
-              <img alt="Thumbnail Why WebSlides?" src="https://webslides.tv/static/images/demos-why.png">
-              <figcaption>
-                <h2>Why WebSlides?</h2>
-              </figcaption>
-            </figure>
-          </a>
-        </li>
-        <li>
-          <a href="../demos/landings.html" title="Landings">
-            <figure>
-              <img alt="Thumbnail Landings" src="https://webslides.tv/static/images/demos-landings.png">
-              <figcaption>
-                <h2>Landings</h2>
-              </figcaption>
-            </figure>
-          </a>
-        </li>
-        <li>
-          <a href="../demos/portfolios.html" title="Portfolios">
-            <figure>
-              <img alt="Thumbnail Portfolios" src="https://webslides.tv/static/images/demos-portfolios.png">
-              <figcaption>
-                <h2>Portfolios</h2>
-              </figcaption>
-            </figure>
-          </a>
-        </li>
-        <li>
-          <a href="../demos/keynote.html" title="Apple Keynote">
-            <figure>
-              <img alt="Thumbnail Apple Keynote" src="https://webslides.tv/static/images/demos-apple.png">
-              <figcaption>
-                <h2>Apple Keynote</h2>
-              </figcaption>
-            </figure>
-          </a>
-        </li>
-      </ul>
-    </div>
-    <!-- .end .wrap -->
-  </section>
+  <!-- Portfolio -->
 
   <section class="aligncenter">
     <div class="wrap">
