a0474df42e22e7fad0657a51f4897d260c543e9e
Arthur Schmitt <tart2000design@gmail.com>
2017-08-30 22:32:29 +0200

    now with dates on events

--- site/templates/event.php
+++ site/templates/event.php
@@ -1,14 +1,13 @@
 <?php snippet('header') ?>
 
 <?php date_default_timezone_set('UTC'); ?>
-<?php setlocale(LC_TIME, "fr_FR", "fra"); ?>
+<?php setlocale(LC_TIME, "fr_FR", ""); ?>
 
 <article>
 	<section>
 		<div class="wrap size-50">
 			<h2><?= $page->title() ?></h2>
-			<?php echo strtotime($page->date()) ?>
-			<p class="subtle"><?= strftime("%A le %d %B, %Y", strtotime($page->date())) ?></p>
+			<p class="date"><?= strftime("%A %d %B %Y", strtotime($page->date('l j F Y'))) ?></p>
 			<?= $page->text()->kirbytext() ?>
 		</div>
 	</section>
