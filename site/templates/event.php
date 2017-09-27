e46aab294b9296aeefa50685f77fcd304a45e716
Arthur Schmitt <tart2000design@gmail.com>
2017-09-27 23:28:53 +0200

    better event pages

--- site/templates/event.php
+++ site/templates/event.php
@@ -4,23 +4,51 @@
 <?php setlocale(LC_TIME, "fr_FR", ""); ?>
 
 <article>
-	<section>
-		<div class="wrap size-50">
-			<h2><?= $page->title() ?></h2>
-			<p class="date">
-				<?php if ($page->date() != ''): ?>
+	<?php if ($page->unsplash() != '') : ?>
+		<section class="bg-black aligncenter">
+		  <span class="background" style="background-image:url('https://source.unsplash.com/<?= $page->unsplash() ?>')"></span>
+		  <div class="wrap">
+		    <h1 class="text-landing text-shadow"><strong><?= $page->title() ?></strong></h1>
+		    <p class="text-shadow bigger-p">
+		      	<?php if ($page->date() != ''): ?>
 					<svg class="fa-calendar">
 			          <use xlink:href="#fa-calendar"></use>
 			        </svg>
 			        <?= strftime("%A %d %B %Y", strtotime($page->date('l j F Y'))) ?>
 				<?php endif ?>
+			</p>
+			<p class="text-shadow bigger-p">
 				<?php if ($page->location() != '' ) : ?>
 					<svg class="fa-map-pin">
 			          <use xlink:href="#fa-map-pin"></use>
 			        </svg>
 			        <?= $page->location() ?>
 				<?php endif ?>
-			</p>
+		    </p>
+		  </div>
+		</section>
+	<?php endif ?>
+
+	<section>
+		<div class="wrap size-50">
+			<?php if ($page->unsplash() == '') : ?>
+				<h2><?= $page->title() ?></h2>
+				<p class="date">
+					<?php if ($page->date() != ''): ?>
+						<svg class="fa-calendar">
+				          <use xlink:href="#fa-calendar"></use>
+				        </svg>
+				        <?= strftime("%A %d %B %Y", strtotime($page->date('l j F Y'))) ?>
+					<?php endif ?>
+					<?php if ($page->location() != '' ) : ?>
+						<svg class="fa-map-pin">
+				          <use xlink:href="#fa-map-pin"></use>
+				        </svg>
+				        <?= $page->location() ?>
+					<?php endif ?>
+				</p>
+			<?php endif ?>
+
 			<?= $page->text()->kirbytext() ?>
 		</div>
 	</section>
